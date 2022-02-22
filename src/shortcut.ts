6f95ae165dd94283086c812b7bf0d405473e99f8
gtm-nayan <gtmnayan@gmail.com>
2022-02-22 17:46:40 +0545

    Fix shortcut action type

--- src/shortcut.ts
+++ src/shortcut.ts
@@ -1,49 +1,54 @@
-import { Action } from './types';
+import { Action } from "./types";
 
 /**
  * Simplest possible way to add a keyboard shortcut to a div or a button.
  * It either calls a callback or clicks on the node it was put on.
- * 
+ *
  * Demo: https://svelte.dev/repl/acd92c9726634ec7b3d8f5f759824d15
  */
 
 export type ShortcutSetting = {
-  control?: boolean;
-  shift?: boolean;
-  alt?: boolean;
+	control?: boolean;
+	shift?: boolean;
+	alt?: boolean;
 
-  code: string;
+	code: string;
 
-  callback?: (node?: HTMLElement) => void;
+	callback?: (node: HTMLElement) => void;
 };
-export const shortcut: Action = (node, params: ShortcutSetting | undefined) => {
-  let handler: ((e: KeyboardEvent) => void) | undefined;
 
-  const removeHandler = () => window.removeEventListener('keydown', handler!),
-    setHandler = (params: ShortcutSetting | undefined) => {
-      removeHandler();
-      if (!params) return;
+export function shortcut(
+	node: HTMLElement,
+	params: ShortcutSetting | undefined
+): ReturnType<Action> {
+	let handler: ((e: KeyboardEvent) => void) | undefined;
 
-      handler = (e: KeyboardEvent) => {
-        if (
-          (!!params.alt != e.altKey) ||
-          (!!params.shift != e.shiftKey) ||
-          (!!params.control != (e.ctrlKey || e.metaKey)) ||
-          params.code != e.code
-        )
-          return;
+	const removeHandler = () => window.removeEventListener("keydown", handler!);
 
-        e.preventDefault();
+	const setHandler = (params: ShortcutSetting | undefined) => {
+		removeHandler();
+		if (!params) return;
 
-        params.callback ? params.callback(node) : node.click();
-      };
-      window.addEventListener('keydown', handler);
-    };
+		handler = (e: KeyboardEvent) => {
+			if (
+				!!params.alt != e.altKey ||
+				!!params.shift != e.shiftKey ||
+				!!params.control != (e.ctrlKey || e.metaKey) ||
+				params.code != e.code
+			)
+				return;
 
-  setHandler(params);
+			e.preventDefault();
 
-  return {
-    update: setHandler,
-    destroy: removeHandler,
-  };
-};
+			params.callback ? params.callback(node) : node.click();
+		};
+		window.addEventListener("keydown", handler);
+	};
+
+	setHandler(params);
+
+	return {
+		update: setHandler,
+		destroy: removeHandler,
+	};
+}
